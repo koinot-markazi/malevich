35a2d742e718db59c27e1176d8883ebd6b124e6c
koinot-markazi <mahkamov4th@gmail.com>
2020-01-15 18:45:56 +0500

    initial commit

---
+++ js/common.js
@@ -0,0 +1,11 @@
+// import Typed from 'typed.js';
+
+var options = {
+  strings: ['Мы создаем шедевры, а хейтеры скажут, что это обычный квадрат...', 'Убираем лишнее. <br>Оставляем необходимое...'],
+  typeSpeed: 40,
+  showCursor: false,
+  loop: true,
+  smartBackspace: true
+};
+
+var typed = new Typed('.m-typer', options);
